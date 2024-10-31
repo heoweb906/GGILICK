b9ddc7f28de5297ac0b9cf3781ffbd7470076ae7
YangH0 <didwogh05@gmail.com>
2024-10-31 15:34:28 +0900

    Squashed commit of the following:
    commit 168e4f87b4215fa09d216cb6cbe3e9d8b5ad395a
    Author: seobindi <seobiiiiii@gmail.com>
    Date:   Thu Oct 31 15:29:52 2024 +0900
    
        v1.1.1
    
        레벨 일부
    
    commit 8afdc2b96fadfe4e07f21076688f8692ccdfb0f6
    Author: seobindi <seobiiiiii@gmail.com>
    Date:   Tue Oct 29 00:33:29 2024 +0900
    
        v1.1.0-alpha1
    
        틀 준비

--- Assets/1__Program/YangJae/TempCameraPos.cs
+++ Assets/1__Program/YangJae/TempCameraPos.cs
@@ -6,19 +6,33 @@ public class TempCameraPos : MonoBehaviour
 {
 
     public GameObject player;
+    public GameObject parentObj;
 
-    [Range(-20, 20)]
+    [Range(-50, 50)]
     public float xPos;
-    [Range(0, 20)]
+    [Range(-50, 50)]
     public float yPos;
-    [Range(-20, 20)]
+    [Range(-50, 50)]
     public float zPos;
 
     void Update()
     {
-        transform.position =
-            new Vector3(player.transform.position.x + xPos,
-            player.transform.position.y + yPos,
-            player.transform.position.z + zPos);
+        transform.localPosition =
+            new Vector3(xPos,
+            yPos,
+            zPos);
+
+        parentObj.transform.position = player.transform.position;
+
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            parentObj.transform.rotation =
+                Quaternion.Euler(new Vector3(parentObj.transform.eulerAngles.x, parentObj.transform.eulerAngles.y - 5, parentObj.transform.eulerAngles.z));
+        }
+        else if(Input.GetKeyDown(KeyCode.P))
+        {
+            parentObj.transform.rotation =
+                Quaternion.Euler(new Vector3(parentObj.transform.eulerAngles.x, parentObj.transform.eulerAngles.y + 5, parentObj.transform.eulerAngles.z));
+        }
     }
 }
